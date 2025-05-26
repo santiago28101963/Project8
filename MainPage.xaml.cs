e5ced1b571f18ba45bc9d25f919c473e756ad143
Santiago <ega2810@gmail.com>
2025-05-26 14:38:48 -0500

    Se comenta el codigo de la pagina de presentacion.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -36,16 +36,16 @@ namespace Project8
             }
 }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
+        //private void OnCounterClicked(object sender, EventArgs e)
+        //{
+        //    count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        //    if (count == 1)
+        //        CounterBtn.Text = $"Clicked {count} time";
+        //    else
+        //        CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        //    SemanticScreenReader.Announce(CounterBtn.Text);
+        //}
     }
 }
